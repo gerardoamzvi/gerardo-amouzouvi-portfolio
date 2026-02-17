5403c110f4c8c10439e9a0f716ae3609a4a75926
juliteGarden <111869677+gerAoMidori@users.noreply.github.com>
2026-02-17 01:11:54 +0100

    SEO optimization github repos caching -- Fix (Test 2)

--- app/page.tsx
+++ app/page.tsx
@@ -35,8 +35,8 @@ export const metadata: Metadata = {
 export const dynamic = "force-static";
 export default async function ProjectsPage() {
 
-  //const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/repos.json`);
-  const res = await fetch("/repos.json");
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/repos.json`);
+  //const res = await fetch("/repos.json");
   const repos = await res.json();
 
   return (
